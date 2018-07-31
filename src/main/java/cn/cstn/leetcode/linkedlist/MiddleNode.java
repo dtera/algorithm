078d5fd237fa2166cd81dae058f46fe349240c6a
zhao <346091714@qq.com>
2018-07-31 08:25:23 +0800

    alter class comment

--- src/main/java/cn/cstn/leetcode/linkedlist/MiddleNode.java
+++ src/main/java/cn/cstn/leetcode/linkedlist/MiddleNode.java
@@ -3,20 +3,20 @@ package cn.cstn.leetcode.linkedlist;
 /**
  * 876                  Middle of the Linked List
  * description :        Given a non-empty, singly linked list with head node head,
- * return a middle node of linked list.
- * If there are two middle nodes, return the second middle node.
+ *                      return a middle node of linked list.
+ *                      If there are two middle nodes, return the second middle node.
  * example 1 :
- * Input: [1,2,3,4,5]
- * Output: Node 3 from this list (Serialization: [3,4,5])
- * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
- * Note that we returned a ListNode object ans, such that:
- * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ *                      Input: [1,2,3,4,5]
+ *                      Output: Node 3 from this list (Serialization: [3,4,5])
+ *                      The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ *                      Note that we returned a ListNode object ans, such that:
+ *                      ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
  * example 2 :
- * Input: [1,2,3,4,5,6]
- * Output: Node 4 from this list (Serialization: [4,5,6])
- * Since the list has two middle nodes with values 3 and 4, we return the second one.
+ *                      Input: [1,2,3,4,5,6]
+ *                      Output: Node 4 from this list (Serialization: [4,5,6])
+ *                      Since the list has two middle nodes with values 3 and 4, we return the second one.
  * note :
- * The number of nodes in the given list will be between 1 and 100.
+ *                      The number of nodes in the given list will be between 1 and 100.
  *
  * @author :           zhaohq
  * date :               2018/7/29 20:38
