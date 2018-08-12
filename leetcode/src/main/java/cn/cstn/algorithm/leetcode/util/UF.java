ac348c44525f5044a51e263339dd805c10438dcc
zhao <346091714@qq.com>
2018-08-12 20:16:35 +0800

    implement UF algorithm

---
+++ leetcode/src/main/java/cn/cstn/algorithm/leetcode/util/UF.java
@@ -0,0 +1,59 @@
+package cn.cstn.algorithm.leetcode.util;
+
+/**
+ * description :        union find
+ * @author :           zhaohq
+ * date :               2018/8/12 0012 18:14
+ */
+public class UF {
+    private int[] id;
+    private int[] sz;
+    private int count;
+
+    public UF(int n) {
+        id = new int[n];
+        sz = new int[n];
+        for (int i = 0; i < n; i++) {
+            id[i] = i;
+            sz[i] = 1;
+        }
+        count = n;
+    }
+
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    public int find(int p) {
+        int r = p;
+        while (r != id[r])
+            r = id[r];
+
+        while (p != id[p]) {
+            int q = id[p];
+            id[p] = r;
+            p = q;
+        }
+
+        return r;
+    }
+
+    public void union(int p, int q) {
+        int pr = find(p);
+        int qr = find(q);
+        if (pr == qr) return;
+        if (sz[pr] < sz[qr]) {
+            id[pr] = qr;
+            sz[qr] += sz[pr];
+        } else {
+            id[qr] = pr;
+            sz[pr] += sz[qr];
+        }
+        count--;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+}
--- leetcode/src/test/java/cn/cstn/algorithm/leetcode/App.java
+++ leetcode/src/test/java/cn/cstn/algorithm/leetcode/App.java
@@ -1,11 +1,55 @@
 package cn.cstn.algorithm.leetcode;
 
+import cn.cstn.algorithm.leetcode.util.ArrayHelper;
+import cn.cstn.algorithm.leetcode.util.UF;
 import org.junit.Test;
 
 public class App {
 
     @Test
-    public void test() {
+    public void testUF() {
+        int[][] a = {
+                {1, 1, 0, 0, 1, 1, 0},
+                {0, 1, 0, 0, 0, 1, 1},
+                {1, 1, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 1, 0, 0},
+                {1, 1, 0, 0, 0, 0, 1},
+                {1, 0, 0, 1, 0, 1, 1},
+                {1, 1, 0, 1, 0, 0, 1}
+        };
+        int m = a.length, n = a[0].length, count = 0;
+        int[][] b = new int[m][n];
+        UF uf = new UF(m * n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 0) {
+                    count++;
+                    continue;
+                }
+                if (i > 0 && j > 0 && a[i - 1][j - 1] == 1) uf.union((i - 1) * n + j - 1, i * n + j);
+                if (i > 0 && a[i - 1][j] == 1) uf.union((i - 1) * n + j, i * n + j);
+                if (i > 0 && j < n - 1 && a[i - 1][j + 1] == 1) uf.union((i - 1) * n + j + 1, i * n + j);
+                if (j > 0 && a[i][j - 1] == 1) uf.union(i * n + j - 1, i * n + j);
+                if (j < n - 1 && a[i][j + 1] == 1) uf.union(i * n + j + 1, i * n + j);
+                if (i < m - 1 && j > 0 && a[i + 1][j - 1] == 1) uf.union((i + 1) * n + j - 1, i * n + j);
+                if (i < m - 1 && a[i + 1][j] == 1) uf.union((i + 1) * n + j, i * n + j);
+                if (i < m - 1 && j < n - 1 && a[i + 1][j + 1] == 1) uf.union((i + 1) * n + j + 1, i * n + j);
+            }
+        }
 
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = uf.find(i * n + j);
+                if (a[i][j] == 0) b[i][j] = 0;
+            }
+        }
+
+        for (int[] i : a) ArrayHelper.println(i);
+        System.out.println("==============================");
+        for (int i = 0; i < m; i++)
+            ArrayHelper.println(b[i]);
+
+        System.out.println(uf.getCount() - count);
+        System.out.println(uf.isConnected(0, 16));
     }
 }
