bf956eede5d23caa6aac0c81565b4bc34e3523e9
zhao <346091714@qq.com>
2018-08-28 21:42:36 +0800

    add SortTest

--- commons/src/test/java/cn/cstn/algorithm/commons/test/SortTest.java
+++ commons/src/test/java/cn/cstn/algorithm/commons/test/SortTest.java
@@ -1,6 +1,5 @@
 package cn.cstn.algorithm.commons.test;
 
-import cn.cstn.algorithm.commons.Search;
 import cn.cstn.algorithm.commons.Sort;
 import org.junit.Test;
 
@@ -10,7 +9,7 @@ import java.util.Map;
 import java.util.Random;
 import java.util.function.Consumer;
 
-public class App {
+public class SortTest {
 
     @Test
     public void sort() {
@@ -20,8 +19,8 @@ public class App {
         sorts.put("冒泡", Sort::bubbleSort);
         sorts.put("归并", Sort::mergeSort);
         sorts.put("快速", Sort::quickSort);
-        sorts.put("基数", App::radixSort);
-        sorts.put("计数", App::countSort);
+        sorts.put("基数", SortTest::radixSort);
+        sorts.put("计数", SortTest::countSort);
         sorts.put("堆", Sort::heapSort);
         sorts.put("桶", Sort::bucketSort);/**/
 
@@ -59,24 +58,5 @@ public class App {
 
         Sort.radixSort(arr, 5, radix);
     }
-
-    @Test
-    public void find() {
-        Integer[] arr = {5, 6, 7, 8, 9, 1, 2, 3, 4};
-        System.out.println("带查找数组：" + Arrays.asList(arr));
-        //Arrays.stream(arr).forEach(p->System.out.print(p+", "));
-
-        System.out.println("================分治查找旋转数组================");
-        for (int a : arr)
-            System.out.println(a + "元素的索引为：" + Search.rotatedArrFind(arr, a));
-
-        Arrays.sort(arr);
-        System.out.println("================二分查找有序数组================");
-        for (int a : arr)
-            System.out.println(a + "元素的索引为：" + Search.BS(arr, a));
-
-        System.out.println(arr[0] + "元素的索引为：" + Search.BS(arr, arr[0], 0, arr.length));
-    }
-
 
 }
