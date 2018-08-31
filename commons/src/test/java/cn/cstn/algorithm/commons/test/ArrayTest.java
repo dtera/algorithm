2a50809fbec0e2edc03a6f61b81a0b5dac39034d
zhao <346091714@qq.com>
2018-08-31 10:24:53 +0800

    add permutation and combination test method to ArrayTest

--- commons/src/test/java/cn/cstn/algorithm/commons/test/ArrayTest.java
+++ commons/src/test/java/cn/cstn/algorithm/commons/test/ArrayTest.java
@@ -1,9 +1,13 @@
 package cn.cstn.algorithm.commons.test;
 
+import cn.cstn.algorithm.commons.math.Point;
 import cn.cstn.algorithm.commons.util.ArrayUtil;
 import org.junit.Test;
 
+import java.util.Comparator;
+
 public class ArrayTest {
+
     @Test
     public void testPrint() {
         int[] a = {4, 6, 1, 8, 5, 3, 9, 7, 2};
@@ -12,5 +16,42 @@ public class ArrayTest {
         ArrayUtil.print(a, ", ", "", "\n");
         ArrayUtil.println(b);
         ArrayUtil.print(b, ", ", "", "\n");
+        ArrayUtil.swap(a, 1, 2);
     }
+
+    @Test
+    public void testReverse() {
+        Integer[] a = {1, 2, 3};
+        ArrayUtil.reverse(a);
+        ArrayUtil.println(a);
+        System.out.println("========================================");
+        ArrayUtil.reverse(a, 1, 2);
+        ArrayUtil.println(a);
+    }
+
+    @Test
+    public void testPermutation() {
+        Integer[] a = {1, 2, 3};
+        ArrayUtil.permutation(a, ArrayUtil::println);
+        System.out.println("========================================");
+        ArrayUtil.permutation(a, 1, 2, ArrayUtil::println);
+        System.out.println("========================================");
+        Integer[] b = {1, 2, 3};
+        ArrayUtil.println(b);
+        System.out.println("next permutation:");
+        while (ArrayUtil.nextPermutation(b, ArrayUtil::println)) System.out.println("next permutation:");
+        System.out.println("========================================");
+        Point[] ps = {new Point(1,3), new Point(2,1), new Point(3,2)};
+        ArrayUtil.println(ps);
+        System.out.println("next permutation:");
+        while (ArrayUtil.nextPermutation(ps, Comparator.comparingInt(Point::getX), ArrayUtil::println)) System.out.println("next permutation:");
+    }
+
+    @Test
+    public void testCombination() {
+        Integer[] a = {1, 2, 3, 4};
+        ArrayUtil.combination(a, null);
+        ArrayUtil.combination(a, 3, System.out::println);
+    }
+
 }
