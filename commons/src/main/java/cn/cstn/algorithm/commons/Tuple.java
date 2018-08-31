0da5be93e3c16de4bb02c8f5ce277e62ac5780db
zhao <346091714@qq.com>
2018-08-31 19:41:08 +0800

    add Tuple

---
+++ commons/src/main/java/cn/cstn/algorithm/commons/Tuple.java
@@ -0,0 +1,30 @@
+package cn.cstn.algorithm.commons;
+
+/**
+ * description :        two tuple
+ * @author :           zhaohq
+ * date :               2018/8/31 0031 17:23
+ */
+public class Tuple<T> {
+    private T _1;
+    private T _2;
+
+    public Tuple(T _1, T _2) {
+        this._1 = _1;
+        this._2 = _2;
+    }
+
+    public T _1() {
+        return _1;
+    }
+
+    public T _2() {
+        return _2;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + _1 + ", " + _2 + ")";
+    }
+
+}
