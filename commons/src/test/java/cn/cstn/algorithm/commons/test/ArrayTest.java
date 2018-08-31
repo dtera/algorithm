59639362fcb79263420f098c973c2d29e72e5538
zhao <346091714@qq.com>
2018-08-31 19:38:36 +0800

    add coCombination test method to ArrayTest

--- commons/src/test/java/cn/cstn/algorithm/commons/test/ArrayTest.java
+++ commons/src/test/java/cn/cstn/algorithm/commons/test/ArrayTest.java
@@ -2,9 +2,13 @@ package cn.cstn.algorithm.commons.test;
 
 import cn.cstn.algorithm.commons.math.Point;
 import cn.cstn.algorithm.commons.util.ArrayUtil;
+import cn.cstn.algorithm.commons.util.CollectionUtil;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class ArrayTest {
 
@@ -41,17 +45,25 @@ public class ArrayTest {
         System.out.println("next permutation:");
         while (ArrayUtil.nextPermutation(b, ArrayUtil::println)) System.out.println("next permutation:");
         System.out.println("========================================");
-        Point[] ps = {new Point(1,3), new Point(2,1), new Point(3,2)};
+        Point[] ps = {new Point(1, 3), new Point(2, 1), new Point(3, 2)};
         ArrayUtil.println(ps);
         System.out.println("next permutation:");
-        while (ArrayUtil.nextPermutation(ps, Comparator.comparingInt(Point::getX), ArrayUtil::println)) System.out.println("next permutation:");
+        while (ArrayUtil.nextPermutation(ps, Comparator.comparingInt(Point::getX), ArrayUtil::println))
+            System.out.println("next permutation:");
     }
 
     @Test
     public void testCombination() {
         Integer[] a = {1, 2, 3, 4};
-        ArrayUtil.combination(a, null);
+        ArrayUtil.combination(a, System.out::println);
+        System.out.println("=============================");
         ArrayUtil.combination(a, 3, System.out::println);
+    }
+
+    @Test
+    public void testCoCombination() {
+        Integer[] a = {1, 2, 3, 4};
+        ArrayUtil.coCombination(a, System.out::println);
     }
 
 }
