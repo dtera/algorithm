0d95c83eb3a66979815b1e5a65b1fc0a9f53e4c5
zhao <346091714@qq.com>
2018-09-03 11:24:32 +0800

    refactor CreditsCardGame

--- corporation/src/main/java/cn/cstn/algorithm/corporation/bytedance/CreditsCardGame.java
+++ corporation/src/main/java/cn/cstn/algorithm/corporation/bytedance/CreditsCardGame.java
@@ -40,8 +40,34 @@ public class CreditsCardGame {
         for (int i = 0; i < n; i++)
             ts[i] = new Tuple<>(sc.nextInt(), sc.nextInt());
 
+        System.out.println(__maxGroupScore(ts));
         System.out.println(_maxGroupScore(ts));
         System.out.println(maxGroupScore(ts));
+    }
+
+    private static int __maxGroupScore(Tuple<Integer>[] ts) {
+        int sx = 0, sy = 0;
+        for (Tuple<Integer> t : ts) {
+            sx += t._1();
+            sy += t._2();
+        }
+        int[][] f = new int[2][sx + 1];
+        Arrays.fill(f[0], - sy - 1);
+        Arrays.fill(f[1], - sy - 1);
+        int[] pc = {0, 1};
+        f[pc[1]][sx] = 0;
+        for (Tuple<Integer> t : ts) {
+            ArrayUtil.swap(pc, 0, 1);
+            for (int j = 0; j < f[0].length; j++) {
+                f[pc[1]][j] = Math.max(f[pc[0]][j], f[pc[0]][Math.abs(j - t._1())] + t._2());
+                if (j + t._1() < f[0].length)
+                    f[pc[1]][j] = Math.max(f[pc[1]][j], f[pc[0]][j + t._1()] + t._2());
+            }
+            log.debug("\n" + t + "\t\tf[0] = " + Arrays.toString(f[0]) + //
+                    "\t\tf[1] = " + Arrays.toString(f[1]));
+        }
+
+        return f[pc[1]][sx];
     }
 
     private static int _maxGroupScore(Tuple<Integer>[] ts) {
@@ -51,22 +77,24 @@ public class CreditsCardGame {
             sy += t._2();
         }
         int[][] f = new int[2][2 * sx + 1];
-        Arrays.fill(f[0], - sy * 4);
-        Arrays.fill(f[1], - sy * 4);
+        Arrays.fill(f[0], - sy - 1);
+        Arrays.fill(f[1], - sy - 1);
         int[] pc = {0, 1};
         f[pc[1]][sx] = 0;
         for (Tuple<Integer> t : ts) {
             ArrayUtil.swap(pc, 0, 1);
             for (int j = 0; j < f[0].length; j++) {
                 f[pc[1]][j] = f[pc[0]][j];
-                log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
+                //log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
                 if (j - t._1() >= 0)
                     f[pc[1]][j] = Math.max(f[pc[1]][j], f[pc[0]][j - t._1()] + t._2());
-                log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
+                //log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
                 if (j + t._1() < f[0].length)
                     f[pc[1]][j] = Math.max(f[pc[1]][j], f[pc[0]][j + t._1()] + t._2());
-                log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
+                //log.debug("f[0][" + j + "] = " + f[0][j] + "\t\tf[1][" + j + "] = " + f[1][j]);
             }
+            log.debug("\n" + t + "\t\tf[0] = " + Arrays.toString(f[0]) + //
+                    "\t\tf[1] = " + Arrays.toString(f[1]));
         }
 
         return f[pc[1]][sx];
