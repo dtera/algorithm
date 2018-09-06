b7eeb3e0b7ef3b55b2b991ef910b9f986d0ecc6a
zhao <346091714@qq.com>
2018-09-06 18:14:36 +0800

    refactor Tuple

--- commons/src/main/java/cn/cstn/algorithm/commons/Tuple.java
+++ commons/src/main/java/cn/cstn/algorithm/commons/Tuple.java
@@ -5,34 +5,10 @@ package cn.cstn.algorithm.commons;
  * @author :            zhaohq
  * date :               2018/8/31 0031 17:23
  */
-public class Tuple<T> {
-    private T _1;
-    private T _2;
+public class Tuple<T> extends KV<T, T> {
 
     public Tuple(T _1, T _2) {
-        this._1 = _1;
-        this._2 = _2;
-    }
-
-    public T _1() {
-        return _1;
-    }
-
-    public T _2() {
-        return _2;
-    }
-
-    public void s_1(T _1) {
-        this._1 = _1;
-    }
-
-    public void s_2(T _2) {
-        this._2 = _2;
-    }
-
-    @Override
-    public String toString() {
-        return "(" + _1 + ", " + _2 + ")";
+        super(_1, _2);
     }
 
 }
