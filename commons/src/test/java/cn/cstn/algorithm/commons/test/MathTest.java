1b10b75ac3e4754883ca6535d898b1fbec1c0b02
zhaohuiqiang <346091714@qq.com>
2018-09-09 23:07:35 +0800

    add MathTest

---
+++ commons/src/test/java/cn/cstn/algorithm/commons/test/MathTest.java
@@ -0,0 +1,13 @@
+package cn.cstn.algorithm.commons.test;
+
+import cn.cstn.algorithm.commons.util.MathUtil;
+import org.junit.Test;
+
+public class MathTest {
+
+    @Test
+    public void testGcd() {
+        System.out.println(MathUtil.gcd(12, 18));
+    }
+
+}
