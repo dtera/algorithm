4f599bdca5e8da2d825d202b08c9659dc6d87bd0
zhaohuiqiang <346091714@qq.com>
2018-09-10 20:34:41 +0800

    add tuple test

--- commons/src/test/java/cn/cstn/algorithm/commons/test/AppTest.java
+++ commons/src/test/java/cn/cstn/algorithm/commons/test/AppTest.java
@@ -1,12 +1,24 @@
 package cn.cstn.algorithm.commons.test;
 
 import cn.cstn.algorithm.commons.KV;
+import cn.cstn.algorithm.commons.Tuple;
 import org.junit.Test;
 
 public class AppTest {
+
     @Test
     public void testKV() {
         KV<Integer, Integer> kv = new KV<>(1, 2);
         System.out.println(kv);
     }
+
+    @Test
+    public void testTuple() {
+        Tuple<Integer> t = new Tuple<>(1, 2, 3, 4);
+        System.out.println(t);
+        System.out.println(t.g_(2));
+        t.s_(2, 9);
+        System.out.println(t.g_(2));
+    }
+
 }
