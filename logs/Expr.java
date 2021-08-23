4a5a3d8fdd4974d5dde928eae7842cf91edbeef0
dterazhao <dterazhao@tencent.com>
2021-08-23 10:26:34 +0800

    [feat]: add compiler test

---
+++ logs/Expr.java
@@ -0,0 +1,17 @@
+package logs;
+
+/**
+ * Expr
+ *
+ * @author dterazhao
+ * @date 2021/8/23
+ */
+public class Expr {
+
+    public static void main(String[] args) {
+        int a = 3;
+        int r = 1 + 2 * a;
+        System.out.println(r);
+    }
+
+}
