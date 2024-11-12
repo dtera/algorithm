f30d2ce4b68f8716cf55b1e498134a709ec74445
dtera <huster1005@gmail.com>
2024-11-12 19:30:09 +0800

    feat: compiler static library for phe_kit

--- bazel/build.sh
+++ bazel/build.sh
@@ -9,10 +9,13 @@ echo "Current Directory: $CD"
 
 cd ..
 OPTS="-c opt"
+SED_F="native/src/main/native/cn/cstn/algorithm/javacpp/heu/BUILD.bazel"
 if [[ "$OSTYPE" == "darwin"* ]]; then
   echo 6.5.0 > .bazelversion
+  sed -i "" 's@^#load("//bazel:cc_static_library.bzl"@load("//bazel:cc_static_library.bzl"@g' "$SED_F"
 else
   echo 7.4.0 > .bazelversion
+  sed -i 's@^load("//bazel:cc_static_library.bzl"@#load("//bazel:cc_static_library.bzl"@g' "$SED_F"
   OPTS="$OPTS --config=exp"
 fi
 # --config=monolithic --spawn_strategy=standalone --genrule_strategy=standalone --output_filter=DONT_MATCH_ANYTHING
