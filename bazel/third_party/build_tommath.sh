4c487bf02e6ba4553d6b0b64080a5cb59e88fb22
dterazhao <dterazhao@tencent.com>
2024-11-19 14:49:36 +0800

    feat: build tommath

--- bazel/third_party/build_tommath.sh
+++ bazel/third_party/build_tommath.sh
@@ -3,6 +3,10 @@
 CD=$(cd "$(dirname "$0")" || exit && pwd)
 cd "$CD" || exit
 . "$CD"/versions.sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # shellcheck disable=SC2155
+  export SDKROOT=$(xcrun --show-sdk-path)
+fi
 
 # build libtommath
 # [ -d libtommath ] || git clone https://github.com/libtom/libtommath.git
@@ -10,7 +14,7 @@ cd "$CD" || exit
 pkg="libtommath-$libtommath_ver"
 download_url=https://github.com/libtom/libtommath/archive/"$libtommath_ver".tar.gz
 sh "$CD"/build_template.sh --pkg "$pkg" -u "$download_url" -o \
-"-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$CD" \
+"-DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_LIBDIR=lib -DCMAKE_INSTALL_PREFIX=$CD" \
 -d false
 
 cp "$pkg"/tommath*.h include/libtommath/
