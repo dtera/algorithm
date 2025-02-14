098c59b2ce62772756cebd7ff61eec2e841c1aa8
dterazhao <dterazhao@tencent.com>
2025-02-14 12:17:59 +0800

    feat: refactor to build bazel

--- bazel/build.sh
+++ bazel/build.sh
@@ -11,11 +11,14 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 elif echo "$OSTYPE" | grep -q "linux" || [[ "$OSTYPE" == "" ]]; then
   os_release=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
   if [[ "$os_release" == "ubuntu" ]]; then
-    sudo apt install -y npm libomp-dev
+    sudo apt update -y && sudo apt install -y wget libomp-dev
+  elif [[ "$os_release" == "alpine" ]]; then
+    echo "alpine linux"
   else
-    sudo yum install -y npm libgomp
+    sudo yum install -y wget libgomp
   fi
-  sudo npm install -g @bazel/bazelisk
+  wget https://github.com/bazelbuild/bazelisk/releases/download/v1.25.0/bazelisk-linux-amd64 -O /usr/local/bin/bazelisk
+  chmod +x /usr/local/bin/bazelisk && ln -s /usr/local/bin/bazelisk /usr/local/bin/bazel
 else
   echo "not supported os type: ${OSTYPE}"
   exit 1
