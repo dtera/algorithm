0b28f2ca3440d6bb8fad9e3706f478c06fdd6cd7
dterazhao <dterazhao@tencent.com>
2025-03-20 18:51:47 +0800

    feat: update llm_pipe and config

--- python/llm_pipe.py
+++ python/llm_pipe.py
@@ -6,7 +6,7 @@ import json
 import os
 
 import yaml
-from transformers import pipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 def load_conf(f_path="config.yml"):
@@ -19,8 +19,31 @@ def load_conf(f_path="config.yml"):
 
 if __name__ == '__main__':
     conf = load_conf()
+
+    model = AutoModelForCausalLM.from_pretrained(
+        conf["model"],
+        torch_dtype="auto",
+        device_map="auto"
+    )
+    tokenizer = AutoTokenizer.from_pretrained(conf["model"])
+
     messages = [
         {"role": "user", "content": conf["prompt"]},
     ]
-    pipe = pipeline(conf["task"], model=conf["model"], trust_remote_code=conf["trust_remote_code"])
-    pipe(messages)
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+    generated_ids = model.generate(
+        **model_inputs,
+        max_new_tokens=32768
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+
+    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    print(response)
