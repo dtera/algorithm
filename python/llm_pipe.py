9fe721444640d614b896b9f1df269be8ad4f69c7
dterazhao <dterazhao@tencent.com>
2025-03-20 10:58:41 +0800

    feat: add llm_pipe and the related dependencies

---
+++ python/llm_pipe.py
@@ -0,0 +1,26 @@
+# coding:utf-8
+"""
+LLM pipeline
+"""
+import json
+import os
+
+import yaml
+from transformers import pipeline
+
+
+def load_conf(f_path="config.yml"):
+    config = {}
+    if os.path.exists(f_path):
+        with open(f_path) as f:
+            config = json.load(f) if f_path.endswith(".json") else yaml.load(f, Loader=yaml.FullLoader)
+    return config
+
+
+if __name__ == '__main__':
+    conf = load_conf()
+    messages = [
+        {"role": "user", "content": conf["prompt"]},
+    ]
+    pipe = pipeline(conf["task"], model=conf["model"], trust_remote_code=conf["trust_remote_code"])
+    pipe(messages)
