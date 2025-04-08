70faaef3daf87c60901d7ed51aa57b81c44bb26b
dterazhao <dterazhao@tencent.com>
2025-04-08 20:12:47 +0800

    feat: update llm_pipe and config

--- python/llm_pipe.py
+++ python/llm_pipe.py
@@ -6,7 +6,7 @@ import json
 import os
 
 import yaml
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import pipeline
 
 
 def load_conf(f_path="config.yml"):
@@ -20,30 +20,10 @@ def load_conf(f_path="config.yml"):
 if __name__ == '__main__':
     conf = load_conf()
 
-    model = AutoModelForCausalLM.from_pretrained(
-        conf["model"],
-        torch_dtype="auto",
-        device_map="auto"
-    )
-    tokenizer = AutoTokenizer.from_pretrained(conf["model"])
-
     messages = [
         {"role": "user", "content": conf["prompt"]},
     ]
-    text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
-    )
-    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-    generated_ids = model.generate(
-        **model_inputs,
-        max_new_tokens=32768
-    )
-    generated_ids = [
-        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-
-    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
-    print(response)
+    pipe = pipeline(conf["task"], model=conf["model"])
+    resp = pipe(messages)
+    for chunk in resp:
+        print(chunk["generated_text"][1]["content"])
