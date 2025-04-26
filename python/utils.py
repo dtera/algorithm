c8472e353fdbbb90316606abdda25ad2476f3d18
dterazhao <dterazhao@tencent.com>
2025-04-26 21:58:35 +0800

    feat(python): update utils.py

--- python/utils.py
+++ python/utils.py
@@ -1,9 +1,14 @@
 # coding:utf-8
-
+import json
 import logging
 import time
 
 import requests
+
+
+def read_line(f_name):
+    with open(f_name, "r") as f:
+        return f.readline()
 
 
 def print_time(f):
@@ -17,6 +22,10 @@ def print_time(f):
     return inner
 
 
+def json_format(s):
+    return json.dumps(s, sort_keys=True, indent=2)
+
+
 def is_empty(a):
     return a is None or len(a.strip() if type(a) == str else a) == 0
 
@@ -25,8 +34,8 @@ def is_not_empty(a):
     return not is_empty(a)
 
 
-def http_req(url: str, payload: dict = None, api_key: str = None, headers: dict = None, out_format: str = 'json',
-             timeout: int = 300):
+def http_req(url: str, payload: dict = None, method: str = "get", api_key: str = None, headers: dict = None,
+             out_format: str = 'json', timeout: int = 300, max_retry=0, retry_interval=5, stream=False):
     _headers = {
         "Content-Type": "application/json"
     }
@@ -35,18 +44,30 @@ def http_req(url: str, payload: dict = None, api_key: str = None, headers: dict 
     if headers is not None:
         _headers.update(headers)
 
-    response = requests.get(url, headers=_headers, timeout=timeout) if payload is None else (
-        requests.post(url, headers=_headers, json=payload, timeout=timeout))
-    http_code = response.status_code
-    if http_code != 200:
-        logging.error(f"ERROR http_code={http_code}: RESPONSE = {response.text}")
-        raise ValueError(f"Error response with http_code={http_code}: RESPONSE = {response.text}.")
-    return response.json() if out_format == "json" else response.text
+    tries = 0
+    resp = {"errcode": -1, 'errmsg': None, 'data': None}
+    retry_interval = max(retry_interval, 5)
+    while tries <= max_retry:
+        tries += 1
+        if tries > 1:
+            time.sleep(retry_interval)
+        response = requests.get(url, headers=_headers, timeout=timeout,
+                                stream=stream) if payload is None and method == "get" else (
+            requests.post(url, headers=_headers, json=payload, timeout=timeout, stream=stream))
+        if response.status_code == 200:
+            out_format = "raw" if stream else out_format
+            return response.json() if out_format == "json" else (response.text if out_format == "text" else response)
+        else:
+            logging.warning(f"ERROR status_code={response.status_code}: RESPONSE = {response.text}")
+            resp["errcode"] = response.status_code
+            resp["errmsg"] = response.reason
+            # raise ValueError(f"Error response with http_code={http_code}: RESPONSE = {response.text}.")
+    return resp
 
 
 if __name__ == "__main__":
     print(is_empty('    '))
     print(is_empty(' aaa'))
     print(is_not_empty('xxx'))
-    print()
-    print(http_req('https://www.baidu.com', out_format='text'))
+    res = http_req("https://gateway.53zaixian.com/resource-bs-api/res/home/findLearningPeriodGrade", method="post")
+    print(json_format(res))
